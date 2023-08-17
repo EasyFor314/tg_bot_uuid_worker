39acfcac0e22a76b1d02570912e29f53df4ef235
Leo <vandi.var@yandex.ru>
2023-08-17 11:17:20 +0300

    Update base_handlers.py

--- handlers/base_handlers.py
+++ handlers/base_handlers.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 from aiogram import types
 
 
@@ -8,3 +9,11 @@ async def bot_start(msg: types.Message):
     except Exception as exception:
         #await msg.answer(f'Упс. что-то пошло не так! Обратитесь к администраторам бота')
         logging.error(f'Exception : {exception}')
+
+async def convert_string_to_uuid(msg: types.Message):
+    try:
+        output_msg = uuid.UUID(msg.text).hex
+        await msg.answer(output_msg)
+    except Exception as error:
+        logging.error('Возникла ошибка {0}'.format(str(error)))
+        await msg.answer("Произошла ошибка, повторите позднее")
