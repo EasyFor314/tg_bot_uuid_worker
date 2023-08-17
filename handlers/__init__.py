709152841989b7af466a2fbd49b6ccc523fd77ec
Leo <vandi.var@yandex.ru>
2023-08-17 11:17:47 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher.filters import CommandStart, CommandHelp, Command
 
-from .base_handlers import bot_start
+from .base_handlers import bot_start, convert_string_to_uuid
 
 def setup(dp: Dispatcher):
     dp.register_message_handler(bot_start, CommandStart())
+    dp.register_message_handler(convert_string_to_uuid)
